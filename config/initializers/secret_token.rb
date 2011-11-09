f2d9c9af58d0723d5bdbabfda81b6f8c36a80b8c
pleasantlight <noam@pleasantlight.com>
2011-11-09 14:11:32 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'ec4980b0645782564619441ae7b3fa0c238b2544bef3e81beadcd804ed3d10aa9fd78ae9f80f5ca59364243e777d3ff143f7a4c6a4ec603f59483a2a21f8d273'
